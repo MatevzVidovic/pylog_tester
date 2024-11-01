95f297a51441ae94b58fdf47f867f9ef29e40a9d
MatevzVidovic <matevz.vidovic@gmail.com>
2024-11-01 23:37:15 +0100

    Testing changes

---
+++ tester.py
@@ -0,0 +1,43 @@
+
+from testing_import import tester
+
+
+import os
+import logging
+import python_logger.log_helper as py_log
+
+MY_LOGGER = logging.getLogger("prototip") # or any string. Mind this: same string, same logger.
+MY_LOGGER.setLevel(logging.DEBUG)
+
+
+python_logger_path = os.path.join(os.path.dirname(__file__), 'python_logger')
+handlers = py_log.file_handler_setup(MY_LOGGER, python_logger_path, add_stdout_stream=False)
+# def file_handler_setup(logger, path_to_python_logger_folder, add_stdout_stream: bool = False)
+
+
+# Add @log(passed_logger=MY_LOGGER) above functions you want to log.
+@py_log.log(passed_logger=MY_LOGGER)
+def forereo(a, b, c):
+    pass
+
+# These automatic logs all contain " @autolog " in their printout.
+
+# Add log_locals(MY_LOGGER) above every return.
+# These logs contain " @log_locals " in its printout instead.
+
+forereo(1, 2, 3)
+
+
+t = tester()
+
+t.test()
+
+@py_log.log(passed_logger=MY_LOGGER)
+def test():
+    print('test function works')
+    t.test()
+
+test()
+
+
+forereo(1, 2, 3)
---
+++ testing_import.py
@@ -0,0 +1,36 @@
+
+
+
+import os
+import logging
+import python_logger.log_helper as py_log
+
+MY_LOGGER = logging.getLogger("prototip") # or any string. Mind this: same string, same logger.
+MY_LOGGER.setLevel(logging.DEBUG)
+
+
+python_logger_path = os.path.join(os.path.dirname(__file__), 'python_logger')
+handlers = py_log.file_handler_setup(MY_LOGGER, python_logger_path, add_stdout_stream=False)
+# def file_handler_setup(logger, path_to_python_logger_folder, add_stdout_stream: bool = False)
+
+
+# Add @log(passed_logger=MY_LOGGER) above functions you want to log.
+@py_log.log(passed_logger=MY_LOGGER)
+def foo(a, b, c):
+    pass
+
+# These automatic logs all contain " @autolog " in their printout.
+
+# Add log_locals(MY_LOGGER) above every return.
+# These logs contain " @log_locals " in its printout instead.
+
+foo(1, 2, 3)
+
+
+
+@py_log.log(passed_logger=MY_LOGGER)
+class tester:
+    def __init__(self):
+        print('tester object created')
+    def test(self):
+        print('testing method works')
